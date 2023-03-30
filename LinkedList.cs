222fbcfbeb0ac82285a3c1950c4d52d3fbdbe978
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-30 13:38:32 +0530

    [Muskan] Added : Csharp program for Linked List.

---
+++ LinkedList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProgram
+{
+    public class LinkedList<T>
+    {
+        private Node<T> head;
+
+        public void Add(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node<T> currentNode = head;
+                while (currentNode.Next != null)
+                {
+                    currentNode = currentNode.Next;
+                }
+                currentNode.Next = newNode;
+            }
+        }
+
+        public void Display()
+        {
+            Node<T> currentNode = head;
+
+            while (currentNode != null)
+            {
+                Console.Write(currentNode.Data + " ");
+                currentNode = currentNode.Next;
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
