9e6c8b93dfd2c8ca0c5211e58134110f611ffa30
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-30 13:32:22 +0530

    [Muskan] Added : Main method in that created intance and added list and display list.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+        }
+    }
+}
