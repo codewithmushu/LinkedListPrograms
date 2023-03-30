5471f7a7adab77206f86e1948540db05632465ed
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-30 13:46:14 +0530

    [Muskan] Added : Changed Sequence of Linked List.

--- Program.cs
+++ Program.cs
@@ -11,9 +11,9 @@ namespace LinkedListProgram
         static void Main(string[] args)
         {
             LinkedList<int> list = new LinkedList<int>();
-            list.Add(56);
-            list.Add(30);
             list.Add(70);
+            list.Add(30);
+            list.Add(56);
             list.Display();
         }
     }
